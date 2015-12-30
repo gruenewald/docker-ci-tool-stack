bf85b6eecfc6cc40f13f53a7718e24f23c5a1dda
Matthias Grünewald <matthias@tmgonline.de>
2015-12-30 11:10:53 +0100

    configure SonarQube to context-path /sonar

--- sonar/run.sh
+++ sonar/run.sh
@@ -6,9 +6,10 @@ if [ "${1:0:1}" != '-' ]; then
 fi
 
 exec java -jar lib/sonar-application-$SONAR_VERSION.jar \
-	-Dsonar.log.console=true \
+  -Dsonar.log.console=true \
   -Dsonar.jdbc.username="$SONARQUBE_JDBC_USERNAME" \
   -Dsonar.jdbc.password="$SONARQUBE_JDBC_PASSWORD" \
   -Dsonar.jdbc.url="$SONARQUBE_JDBC_URL" \
   -Dsonar.web.javaAdditionalOpts="-Djava.security.egd=file:/dev/./urandom" \
+  -Dsonar.web.context="SONARQUBE_RELATIVE_URL_ROOT" \
 	"$@"
